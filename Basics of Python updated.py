ff4249631c2a6e2d3fe3a4940947ee9007be1553
Navjit2023 <147421937+Navjit2023@users.noreply.github.com>
2023-11-10 15:43:21 +0530

    Add files via upload

---
+++ Basics of Python updated.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Sets
+
+# In[1]:
+
+
+sample_set={1,2,3,4,5,1,12.5,7.5,5,5,"kaynen","hi","Navjit","Sonia"}
+
+
+# In[2]:
+
+
+sample_set
+
+
+# In[4]:
+
+
+sample_set.add(555)
+
+
+# In[5]:
+
+
+sample_set
+
+
+# In[6]:
+
+
+sample_set.remove(12.5)
+
+
+# In[7]:
+
+
+sample_set
+
+
+# In[8]:
+
+
+dict_1={'name':"Navjit","hobbies":["music","games","sleeping"],1:25}
+
+
+# In[9]:
+
+
+dict_1
+
+
+# In[10]:
+
+
+dict_1.keys()
+
+
+# In[11]:
+
+
+dict_1.values()
+
+
+# In[12]:
+
+
+dict_1['name']="Sonia"
+
+
+# In[13]:
+
+
+dict_1
+
+
+# In[14]:
+
+
+del dict_1["name"]
+
+
+# In[15]:
+
+
+dict_1
+
+
+# In[17]:
+
+
+dict_1.update({"class":"Data Science"})
+
+
+# In[18]:
+
+
+dict_1
+
+
+# In[ ]:
+
+
+
+
